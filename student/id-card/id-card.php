70927d651099c03aec6bcdafcc5031b2169a3af6
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2025-12-28 23:31:52 +0530

    updated id card left margin

--- student/id-card/id-card.php
+++ student/id-card/id-card.php
@@ -75,7 +75,7 @@ $full_address = ($student['address'] ?? '') . ' ' . $address;
         .card-content {
             position: absolute;
             top: 175px; /* Moved down to be below the blue ribbon */
-            left: 360px; /* Adjusted to align with whitespace */
+            left: 260px; /* Adjusted to align with whitespace */
             width: 280px;
             color: #0f172a;
             font-size: 14px;
