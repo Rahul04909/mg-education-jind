802ace9d4c953353add14396339ade70a198a061
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2025-12-28 18:09:45 +0530

    updated center dashboard sidebar

--- center/sidebar.php
+++ center/sidebar.php
@@ -411,75 +411,18 @@
                     <svg class="nav-chevron" viewBox="0 0 24 24"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
                 <ul class="submenu">
-                    <li class="submenu-item"><a href="#">All Students</a></li>
-                    <li class="submenu-item"><a href="#">New Admission</a></li>
+                    <li class="submenu-item"><a href="../../students/index.php">All Students</a></li>
+                    <li class="submenu-item"><a href="../../students/add-student.php">New Admission</a></li>
                     <li class="submenu-item"><a href="#">Attendance</a></li>
                     <li class="submenu-item"><a href="#">ID Cards</a></li>
                 </ul>
             </li>
 
-            <!-- Batches Dropdown -->
-            <li class="nav-item">
-                <a href="#" class="nav-link" onclick="toggleMenu(this)">
-                    <svg class="nav-icon" viewBox="0 0 24 24"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                    <span class="nav-text">Batches</span>
-                    <svg class="nav-chevron" viewBox="0 0 24 24"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                </a>
-                <ul class="submenu">
-                    <li class="submenu-item"><a href="#">Active Batches</a></li>
-                    <li class="submenu-item"><a href="#">Create Batch</a></li>
-                    <li class="submenu-item"><a href="#">Timetable</a></li>
-                </ul>
-            </li>
-
             <!-- Courses -->
             <li class="nav-item">
-                <a href="http://localhost:3000/mg-skill/center/courses/index.php" class="nav-link">
+                <a href="../../courses/index.php" class="nav-link">
                     <svg class="nav-icon" viewBox="0 0 24 24"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg>
                     <span class="nav-text">Courses</span>
-                </a>
-            </li>
-
-            <!-- Exams -->
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <svg class="nav-icon" viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                    <span class="nav-text">Exams</span>
-                </a>
-            </li>
-        </ul>
-
-        <div class="nav-label" style="margin-top: 24px;">Reports</div>
-        <ul class="nav-list">
-             <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <svg class="nav-icon" viewBox="0 0 24 24"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
-                    <span class="nav-text">Analytics</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <svg class="nav-icon" viewBox="0 0 24 24"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg>
-                    <span class="nav-text">Certificates</span>
-                </a>
-            </li>
-        </ul>
-                <li class="nav-item">
-                    <a href="students/index.php" class="nav-link">
-                        <svg class="nav-icon" viewBox="0 0 24 24"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                        <span class="nav-text">Manage Students</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="manage-fees.php" class="nav-link">
-                         <svg class="nav-icon" viewBox="0 0 24 24"><path d="M12 1v22"/><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/></svg>
-                        <span class="nav-text">Fee Management</span>
-                    </a>
-                </li>
-            <li class="nav-item">
-                <a href="exam-schedule/exam-schedule.php" class="nav-link">
-                    <svg class="nav-icon" viewBox="0 0 24 24"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                    <span class="nav-text">Exam Schedule</span>
                 </a>
             </li>
             
