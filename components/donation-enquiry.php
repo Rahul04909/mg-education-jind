190bac78aefb809477e8b7c7beab7cf7ab88a2b6
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-05 00:28:45 +0530

    updated dropdown in donation form

--- components/donation-enquiry.php
+++ components/donation-enquiry.php
@@ -151,6 +151,18 @@
         border-color: #1358db;
         box-shadow: 0 0 0 3px rgba(19, 88, 219, 0.1);
     }
+    
+    select.form-control {
+        appearance: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='%23374151' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M6 9l6 6 6-6'/%3E%3C/svg%3E");
+        background-repeat: no-repeat;
+        background-position: right 16px center;
+        background-size: 16px;
+        padding-right: 40px;
+        cursor: pointer;
+    }
 
     .form-row {
         display: flex;
