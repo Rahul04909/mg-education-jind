27c61c9383fa8545456c671c2a77d66e0700b41f
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-23 14:40:15 +0530

    updated university section

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ $ogImage=$origin.'assets/images/mg-logo.jpg';
   <?php require_once __DIR__.'/components/courses-grid.php'; ?>
   <?php require_once __DIR__.'/components/blog-component.php'; ?>
   <?php require_once __DIR__.'/components/volunteer-showcase.php'; ?>
+  <?php require_once __DIR__.'/components/universities-component.php'; ?>
   <?php require_once __DIR__.'/components/donation-enquiry.php'; ?>
   <?php require_once __DIR__.'/components/logo-slider.php'; ?>
   <?php require_once __DIR__.'/includes/footer.php'; ?>
