acb334aba88eec080149b1fe8dd7d13c3c5fb7ce
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-04 23:24:06 +0530

    updated reception dashboard

--- reception/index.php
+++ reception/index.php
@@ -51,6 +51,36 @@ usort($recent_activities, function($a, $b) {
 $recent_activities = array_slice($recent_activities, 0, 5);
 
 
+
+function time_elapsed_string($datetime, $full = false) {
+    if ($datetime == '0000-00-00 00:00:00') return "N/A";
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
