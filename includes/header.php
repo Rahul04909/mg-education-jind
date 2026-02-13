b45f2229ee208f783330c6568b4cb2c3c13cb8ef
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-13 22:52:03 +0530

    updated frontend register page

--- includes/header.php
+++ includes/header.php
@@ -144,7 +144,7 @@ body.noscroll{overflow:hidden}
         <svg class="icon" viewBox="0 0 24 24" style="stroke: #ca8a04;"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
         DONATE NOW
       </a>
-      <a href="/register" class="btn btn-primary">REGISTER</a>
+      <a href="../../pages/join-as-a-volunteer" class="btn btn-primary">REGISTER</a>
       <a href="../../admin" class="btn btn-light">Admin Login</a>
       <a href="../../center" class="btn btn-light">Center Login</a>
       <a href="../../student" class=" btn btn-light"> Student Login</a>
