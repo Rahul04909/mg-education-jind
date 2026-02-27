2b78ea766dbc7fd9123906da984bd4b2c9dd3506
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-27 11:15:57 +0530

    updated student dashboard

--- student/sidebar.php
+++ student/sidebar.php
@@ -169,19 +169,19 @@
                 </li>
                 <li>
                     <a href="../../student/date-sheet.php" class="s-nav-link">
-                        <svg class="s-icon" viewBox="0 0 24 24"><path d="M12 1v22"/><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/></svg>
+                        <svg class="s-icon" viewBox="0 0 24 24"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                         Date Sheet
                     </a>
                 </li>
                 <li>
                     <a href="../../student/hall-ticket.php" class="s-nav-link">
-                        <svg class="s-icon" viewBox="0 0 24 24"><path d="M12 1v22"/><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/></svg>
+                        <svg class="s-icon" viewBox="0 0 24 24"><path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z"></path><line x1="13" y1="5" x2="13" y2="19"></line></svg>
                         Hall Ticket
                     </a>
                 </li>
                 <li>
                     <a href="../../online-exam/index.php" class="s-nav-link">
-                        <svg class="s-icon" viewBox="0 0 24 24"><path d="M12 1v22"/><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/></svg>
+                        <svg class="s-icon" viewBox="0 0 24 24"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="2" y1="20" x2="22" y2="20"></line></svg>
                         Online Exam
                     </a>
                 </li>
